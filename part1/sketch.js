faa1f13238c09a25598f0bf3778a0a2ee94cd066
thorpelucy0@gmail.com <lthorpe@deloitte.co.uk>
2018-10-18 14:01:29 +0100

    part change now 'for' loop

--- part1/sketch.js
+++ part1/sketch.js
@@ -6,14 +6,8 @@ function setup() {
 function draw() {
   var x = 10
   var count = 5
-
-  var i = 0
-  while(i < count) {
-    rect(x, 10, 10, 10) // the position and the size of the shape
-    x = x + 20 // 10 + 20
-
-    i = i +1
-    // i is increased until it hits the count.
-
+  for(var i = 0; i < count; i=i+1) {
+    rect(x,10,10,10)
+    x= x+ 20
   }
 }
